5a224468a2aaed4c528238a3ad5b710654d714fd
Renz Endic <endicrenz24@gmail.com>
2026-04-29 14:04:43 +0800

    code: handle all user activity

---
+++ main.py
@@ -0,0 +1,39 @@
+import argparse
+import requests
+
+github_api_url = "https://api.github.com/"
+def get_user_activity(username):
+    user_event_url = f"{github_api_url}users/{username}/events"
+    response = requests.get(user_event_url)
+
+    if response.status_code == 200:
+        user_data = response.json()
+        print('Data retrieved successfully!')
+        return user_data
+        
+    else:
+        print('Failed to retrieve data. Status code:', response.status_code)
+
+def main():
+    parser = argparse.ArgumentParser(description='Github User Activity')
+
+    parser.add_argument('-username', type=str, required=True, help='Github username')
+
+    args = parser.parse_args()
+
+    if args.username != None:
+        username = args.username
+        user_data = get_user_activity(username)
+
+    print(f'{user_data}')
+
+#iterate user_data(list) values
+    for event in user_data:
+        #get specific value (type) and check if its a CreateEvent
+        if event['type'] == 'CreateEvent':
+            print(f'{username} Created a branch at {event["repo"]["name"]}')
+
+        elif event['type'] == 'PushEvent':
+            print(f'{username} Pushed to {event["payload"]} at {event["repo"]["name"]}')
+if __name__ == "__main__":
+    main()
